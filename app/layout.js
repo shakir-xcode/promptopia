56c07a8a3f70a1f23332ba6ee4f95c8928fbfe70
Shakir <shaker88999@gmail.com>
2024-08-16 08:49:09 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import '@styles/globals.css'
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover and Share Prompts',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          {children}
+        </main>
+
+      </body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+const page = () => {
+    return (
+        <h1 className=' '>Home Page</h1>
+    )
+}
+
+export default page
